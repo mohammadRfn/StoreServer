e9f13859923f635efc1c7601c8938c3d8e2035d0
Mohammad <mohammad.rfn98@gmail.com>
2026-09-23 14:17:08 +0330

    mw

--- Modules/AuditLog/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-auditlog::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('auditlog.name') !!}</p>
-</x-auditlog::layouts.master>
--- Modules/AuditLog/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\AuditLog\Http\Controllers\AuditLogController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('auditlogs', AuditLogController::class)->names('auditlog');
-});
